8e8834412ad3f8b9cc8ebef7ebc5137d2632aeef
Nevysha <me@nevysha.art>
2023-05-10 21:18:23 +0200

    initial

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    minify: false,
+    outDir: '../Cozy-Nest/cozy-nest-image-browser',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  base: 'file=extensions/Cozy-Nest/cozy-nest-image-browser',
+})
